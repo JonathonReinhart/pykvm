163fb93145e79f0b76b1a01d8ba82c25de6b9d85
Jonathon Reinhart <Jonathon.Reinhart@gmail.com>
2015-01-13 03:14:38 -0500

    Add basic build for code (aka firmware)

---
+++ misc/entry_code.s
@@ -0,0 +1,28 @@
+[BITS 16]
+size equ (64<<10)
+
+start:
+    mov dx, 0xDEAD
+    in  eax, dx
+    out dx, eax
+    jmp $
+
+
+times (0xFFF0 - ($ - start)) db 0 
+
+reset_vector:
+    jmp     start
+    db 0
+    db 0
+    db 0
+    db 0
+    db 0
+    db 0
+    db 0
+    db 0
+    db 0
+    db 0
+    db 0
+    db 0
+    db 0
+
